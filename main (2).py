380cfb055f2168d51b2ce3a5e4297651fa82b108
TimberWolf <124170004+T1mberWolf@users.noreply.github.com>
2023-02-14 11:17:09 +0300

    IBS255
    14feb 
    1 практическая

---
+++ main (2).py
@@ -0,0 +1,14 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = "no"
+big_netseloe = tseloe * 3.5
+drobnoe -= 1
+tseloe/drobnoe
+big_netseloe/drobnoe
+stroka = "no"*2 + '_' + "yes"*3
+
+print (drobnoe)
+print (big_netseloe)
+print (stroka)
+print (drobnoe)
+print (tseloe)
